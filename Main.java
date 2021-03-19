17dcdeec1a1c053962a46040bc130a2ebc76a8cf
takamuio <68620881+takamuio@users.noreply.github.com>
2021-03-19 13:22:07 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+package com.mycompany.desafionumeros;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        ArrayList<Integer> numeros = new ArrayList();
+
+        int variavel = 0;
+        int index = 0;
+        
+
+        System.out.println("Digite um numero: ");
+        variavel = ler.nextInt();
+
+        do {
+            int membro = variavel % 10;
+            numeros.add(membro);
+            variavel = variavel / 10;
+        } while (variavel != 0);
+
+        do {
+            int decimal = (int) Math.pow(10, index) * numeros.get(index);
+            variavel = decimal;
+            if (index == 0) {
+
+            } else {
+                numeros.set(index, variavel);
+            }
+            index++;
+        } while (index < numeros.size());
+
+        java.util.Collections.reverse(numeros);
+        System.out.println(numeros);
+
+    }
+}
